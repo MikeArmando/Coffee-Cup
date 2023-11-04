41e7ac54d129577844ae7a18362937ebb29faa53
Mike Armando <mike.armando.m@gmail.com>
2023-11-04 11:15:17 -0700

    Update

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -11,7 +11,7 @@ export const Navbar = () => {
     return (
         <>
             <nav>
-                <Link to="/coffee-cup">
+                <Link to="/">
                     <img src={Logo} alt=""></img>
                 </Link>
                 <ul class="nav-item-box">
