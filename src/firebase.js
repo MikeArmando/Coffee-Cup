1dba047e957b8ba643516471c4bfd22f3516a333
Mike Armando <mike.armando.m@gmail.com>
2023-11-07 19:56:26 -0800

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCaxDW7xsuW9mZKtZymcjEBkGhSuJ4zg1Y",
   authDomain: "coffeecup-d736c.firebaseapp.com",
@@ -17,3 +14,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
